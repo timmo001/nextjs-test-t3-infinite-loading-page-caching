645b9725ec08617f840f288482ec1b4ba3dcd78b
Aidan Timson <aidan@timmo.dev>
2024-10-19 13:00:00 +0100

    Pages

--- src/app/collection/_components/product-list.tsx
+++ src/app/collection/_components/product-list.tsx
@@ -1,21 +1,50 @@
 "use client";
 import { Fragment, useEffect } from "react";
 import { InView } from "react-intersection-observer";
+import { parseAsInteger, parseAsStringEnum, useQueryState } from "nuqs";
 import Link from "next/link";
 
 import { api } from "~/trpc/react";
+import { QUERY_LIMIT } from "~/lib/constants";
 import { ProductCard } from "~/components/product/card";
 import { Button } from "~/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "~/components/ui/select";
 
 export default function ProductList() {
+  const [page, setPage] = useQueryState<number>(
+    "page",
+    parseAsInteger.withDefault(1),
+  );
+
+  const [sortKey, setSortKey] = useQueryState<"handle" | "title" | "price">(
+    "sortKey",
+    parseAsStringEnum(["handle", "title", "price"]).withDefault("handle"),
+  );
+
   const productQuery = api.collection.getProducts.useInfiniteQuery(
     {
-      limit: 80,
+      sortKey,
+      limit: QUERY_LIMIT,
+      page,
     },
     {
+      queryKeyHashFn: () => [sortKey, QUERY_LIMIT].join(":"),
+      getPreviousPageParam: (firstPage) => firstPage.previousCursor,
       getNextPageParam: (lastPage) => lastPage.nextCursor,
     },
   );
+
+  async function onPreviousPage() {
+    console.log("Fetching previous page");
+    await productQuery.fetchPreviousPage();
+    console.log("Fetched previous page");
+  }
 
   async function onNextPage() {
     console.log("Fetching next page");
@@ -25,6 +54,8 @@ export default function ProductList() {
 
   useEffect(() => {
     console.table({
+      pages: productQuery.data?.pages.map((page) => page.page).join(","),
+      page,
       "productQuery.data?.pages.length": productQuery.data?.pages.length,
       "productQuery.data?.pageParams.length":
         productQuery.data?.pageParams.length,
@@ -41,6 +72,7 @@ export default function ProductList() {
       "productQuery.hasNextPage": productQuery.hasNextPage,
     });
   }, [
+    page,
     productQuery.data?.pages,
     productQuery.data?.pageParams.length,
     productQuery.isLoading,
@@ -54,23 +86,58 @@ export default function ProductList() {
 
   return (
     <>
-      <div className="grid w-full max-w-screen-lg grid-cols-4 gap-3 p-3">
-        {productQuery.data?.pages.map((page, pageIndex) => (
-          <Fragment key={pageIndex}>
-            <InView
-              className="col-span-full h-0"
-              onChange={(inView) => {
-                if (inView) {
-                  console.log("Page", pageIndex + 1, "is in view");
-                }
-              }}
-            />
+      <div className="mb-5 flex w-full max-w-screen-lg flex-col items-center justify-center p-3">
+        <Select
+          value={sortKey}
+          onValueChange={async (v: "handle" | "title" | "price") => {
+            await setSortKey(v);
+          }}
+        >
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Theme" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="handle">Sort by handle</SelectItem>
+            <SelectItem value="title">Sort by title</SelectItem>
+            <SelectItem value="price">Sort by price</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
+
+      {productQuery.isFetchingPreviousPage ? (
+        <div className="w-full py-5 text-center text-lg font-light">
+          Loading...
+        </div>
+      ) : (
+        productQuery.hasPreviousPage && (
+          <Button onClick={onPreviousPage}>Load previous</Button>
+        )
+      )}
+      <div className="flex w-full max-w-screen-lg flex-col p-3">
+        {productQuery.data?.pages.map((page) => (
+          <InView
+            key={page.page}
+            className="grid grid-cols-4 gap-3"
+            onChange={async (inView) => {
+              console.log(
+                "Page",
+                page.page,
+                inView ? "in view" : "out of view",
+              );
+              if (inView) {
+                await setPage(page.page ?? 0);
+              }
+            }}
+          >
+            <h3 className="col-span-full pb-4 pt-5 text-center text-xl font-semibold">
+              Page {page.page}
+            </h3>
             {page.products.map((product) => (
               <Link key={product.handle} href={`/product/${product.handle}`}>
                 <ProductCard product={product} />
               </Link>
             ))}
-          </Fragment>
+          </InView>
         ))}
       </div>
       {productQuery.isLoading ? (
--- src/server/api/routers/collection.ts
+++ src/server/api/routers/collection.ts
@@ -1,35 +1,54 @@
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import { z } from "zod";
 
+import { QUERY_LIMIT } from "~/lib/constants";
 import { type Product } from "~/lib/schema/product";
 
-const items: Array<Product> = Array.from({ length: 400 }).map((_, i) => {
-  const idPadded = (i + 1).toString().padStart(3, "0");
-  return {
-    handle: `item-${idPadded}`,
-    name: `Item ${idPadded}`,
-    price: Math.floor(Math.random() * 1000),
-  };
-});
+const items: Array<Product> = Array.from({ length: QUERY_LIMIT * 12 }).map(
+  (_, i) => {
+    const idPadded = (i + 1).toString().padStart(3, "0");
+    return {
+      handle: `item-${idPadded}`,
+      name: `Item ${idPadded}`,
+      price: Math.floor(Math.random() * 1000),
+    };
+  },
+);
 
 export const collectionRouter = createTRPCRouter({
   getProducts: publicProcedure
     .input(
       z.object({
+        sortKey: z.string().default("handle"),
         limit: z.number().default(100),
         cursor: z.string().nullish(),
+        page: z.number().nullish(),
       }),
     )
     .query(({ input }) => {
       console.log("getProducts:", input);
 
-      const start = input.cursor
-        ? items.findIndex((item) => item.handle === input.cursor) + 1
+      let cursor: string | null = input.cursor ?? null;
+
+      // If we have a page number, but no cursor, we need to calculate the cursor
+      if (!cursor && input.page && input.page > 0) {
+        cursor = items[(input.page - 1) * input.limit]?.handle ?? null;
+      }
+
+      const start = cursor
+        ? items.findIndex((item) => item.handle === cursor) + 1
         : 0;
       const result = items.slice(start, start + input.limit);
 
       return {
         products: result,
+        page: typeof cursor === "number" ? cursor / input.limit + 1 : 1,
+        previousCursor:
+          !input.page || input.page < 1
+            ? null
+            : start === 0
+              ? null
+              : (result[0]?.handle ?? null),
         nextCursor:
           start + input.limit >= items.length
             ? null
