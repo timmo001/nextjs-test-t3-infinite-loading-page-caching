86c00109ec482765c9e38e0399bc058675b37a42
Aidan Timson <aidan@timmo.dev>
2024-10-20 15:22:39 +0100

    Setup

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -66,7 +66,6 @@ export function Navigation() {
                     className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
                     href="/"
                   >
-                    <Icons.logo className="h-6 w-6" />
                     <div className="mb-2 mt-4 text-lg font-medium">
                       shadcn/ui
                     </div>
